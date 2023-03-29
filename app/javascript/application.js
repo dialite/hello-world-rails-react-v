02b6f16c44c286a075249fca3aac87f7c0e5b43b
dialite <raymondolisa01@gmail.com>
2023-03-29 21:13:06 +0100

    Install babel features

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,13 +3,26 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/configureStore";
+import { NavLink } from "react-router-dom";
 import Greeting from "./controllers/components/Greeting";
 
-const App = () => <Greeting />;
+const App = () => {
+  return (
+    <Routes>
+      <Route exact path="/greet" element={<Greeting />} />
+    </Routes>
+  );
+};
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      <NavLink to="/" role="button">
+        <span>Home | </span>
+      </NavLink>
+      <NavLink to="/greet" role="button">
+        <span>Greet someone</span>
+      </NavLink>
       <App />
     </BrowserRouter>
   </Provider>,
--- app/javascript/redux/slices/greetingsSlice.js
+++ app/javascript/redux/slices/greetingsSlice.js
@@ -1,38 +1,37 @@
-import axios from "axios";
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
-const BASE_URL = "http://localhost:3000/api/v1/greetings";
-const http = axios.create({ baseURL: BASE_URL });
-
-const initialState = {
-  isLoading: false,
-  isFaild: false,
-  greetings: [],
-};
-
+// Update the fetchGreetingsThunk to extract the greeting attribute from the API response
 export const fetchGreetingsThunk = createAsyncThunk(
   "greetings/fetchGreetings",
   async () => {
-    const { data } = await http.get();
-    return data.greeting;
+    const response = await http.get();
+    return response.data.greeting; // extract greeting attribute directly
   }
 );
 
+// Update the initial state to hold a single greeting string instead of an array
+const initialState = {
+  isLoading: false,
+  isFailed: false,
+  greeting: [], // hold a single greeting string instead of an array
+};
+
+// Update the extraReducers to set the greeting attribute directly
 const greetingsSlice = createSlice({
   name: "greetings",
   initialState,
-  extraReducers: {
-    [fetchGreetingsThunk.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.greetings = action.payload;
-    },
-    [fetchGreetingsThunk.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [fetchGreetingsThunk.rejected]: (state) => {
-      state.isFaild = true;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreetingsThunk.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchGreetingsThunk.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isFailed = false;
+        state.greeting = action.payload; // set greeting attribute directly
+      })
+      .addCase(fetchGreetingsThunk.rejected, (state) => {
+        state.isLoading = false;
+        state.isFailed = true;
+      });
   },
 });
-
-export default greetingsSlice.reducer;

4c5a51a564bad843981317c001bf30b72d68c9c9
dialite <raymondolisa01@gmail.com>
2023-03-29 16:06:47 +0100

    Add greetings api controller and add api to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
 end
 
 

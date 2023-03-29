18c5dfd145212c229a533962ec943395d93ee6a6
dialite <raymondolisa01@gmail.com>
2023-03-29 15:53:55 +0100

    Refactor app.js to ES6

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+const App = () => <h1>Hello, World</h1>;
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById("root"));

049dc199b7e34c21b26606c524897895f9a48aac
dialite <raymondolisa01@gmail.com>
2023-03-29 16:29:01 +0100

    Fix rubocop errors

--- Gemfile
+++ Gemfile
@@ -72,5 +72,3 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
-
-gem 'rubocop', '>= 1.0', '< 2.0'
--- app/controllers/api/v1/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::GreetingsController < ApplicationController
-  def index
-    @greetings = Greeting.all.sample
-    render json: {greeting: @greetings}
-  end
-end
